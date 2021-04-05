9ab340af20e12ea1107d7be0324f5cb703d22a3f
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-04-05 10:37:05 -0400

    index js and styles

--- server.js
+++ server.js
@@ -6,12 +6,26 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('?'));
+app.use(express.static('/'));
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/home.html'));
+});
+
+app.get('/home', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/home.html'));
+});
+
+app.get('/tables', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/tables.html'));
+});
+
+app.get('/reserve', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/reserve.html'));
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
-
-// /home
 
 // /reserve
 
